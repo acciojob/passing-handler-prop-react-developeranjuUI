884da63fe6ce8cec6bda773dd28a3254a3bd3114
anju006 <developeranju006@gmail.com>
2025-05-05 22:52:52 +0530

    react exercise 3

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,19 @@
 
+import React, { useState } from 'react';
+import '../styles/Child.css';
+
+const Selection = ({ applyColor }) => {
+    const [ style, setStyle ] = useState({ background: '' });
+
+    const handleClick = () => {
+        applyColor(setStyle);
+    }
+
+    return (
+        <div className='fix-box' style={style} onClick={handleClick}>
+            Selection
+        </div>
+    )
+}
+
+export default Selection;
